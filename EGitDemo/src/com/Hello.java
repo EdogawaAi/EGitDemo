03a75c0e8b964e0986796b2e151e637cedaed18d
tian@desktop <tian_zifeng@hotmail.com>
2016-06-29 02:14:28 -0400

    First implementation of the application.
    Signed-off-by: tian@desktop <tian_zifeng@hotmail.com>

---
+++ EGitDemo/src/com/Hello.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Hello {
+
+	/**
+	 * Implement a sample application.
+	 * @param args arguments
+	 */
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+	
+}
